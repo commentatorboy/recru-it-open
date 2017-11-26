d5b95e5b971f6bd822a82dc4b7763a6f7f1ec4d5
howiep <hawkarp@gmail.com>
2017-11-26 23:51:24 +0100

    fresh start

---
+++ Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace recru_it.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
