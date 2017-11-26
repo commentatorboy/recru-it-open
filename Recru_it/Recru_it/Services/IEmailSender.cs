223ac04e147915115d5b397d4402435a3d0a92f4
howiep <hawkarp@gmail.com>
2017-11-26 23:09:34 +0100

    1

---
+++ Recru_it/Recru_it/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recru_it.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
