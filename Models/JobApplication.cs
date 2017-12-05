11d6880e7610e2cc9cd5ed8bc00966efc25af8b2
hamun munikiki <commentatorbot@gmail.com>
2017-12-05 23:01:55 +0100

    Added other variable type

---
+++ Models/JobApplication.cs
@@ -0,0 +1,23 @@
+﻿using recru_it.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace recru_it.Models
+{
+    public class JobApplication
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string CreatedBy { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public Tag Tags { get; set; }   
+    }
+}
